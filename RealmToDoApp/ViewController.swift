39572cc12560df789277af89748f12bb8fc50b4e
Kenzo Nirasawa <kemuzo.nira@gmail.com>
2017-01-08 20:25:38 +0900

    Initial Commit

---
+++ RealmToDoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmToDoApp
+//
+//  Created by Kenzo on 1/8/17.
+//  Copyright © 2017 RealmToDoApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
